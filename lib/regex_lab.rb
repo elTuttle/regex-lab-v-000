6a78f7cf48b03cb7dcdea73827065b32d53027a4
John Tuttle <thejwt@gmail.com>
2017-09-27 04:17:46 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,7 +23,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.match(/^\W?\d{3}\W?\d{3}\W?\d{4}$/)
 end
 
 #starts_with_a_vowel?("egg")
